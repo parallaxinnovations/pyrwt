6a3d20e03610d00cd53d24242c42541058993384
Amit Aides (chiche computer) <Amit Aides (chiche computer)@localhost>
2012-08-25 22:18:42 +0300

    Documentation.

--- rwt/daubcqf.py
+++ rwt/daubcqf.py
@@ -5,29 +5,32 @@ import scipy as sp
 
 def daubcqf(N, TYPE='min'):
     """
-    Function computes the Daubechies' scaling and wavelet filters
-    (normalized to sqrt(2)).
+    Computes the Daubechies' scaling and wavelet filters (normalized to sqrt(2)).
 
-    Input: 
-       N    : Length of filter (must be even)
-       TYPE : Optional parameter that distinguishes the minimum phase,
-              maximum phase and mid-phase solutions ('min', 'max', or
-              'mid'). If no argument is specified, the minimum phase
-              solution is used.
+    Parameters
+    ----------
+    N : int
+        Length of filter (must be even)
+    TYPE : ['min', 'max', 'mid'], optional (default='min')
+        Distinguishes the minimum phase, maximum phase and mid-phase solutions
+        ('min', 'max', or 'mid').
+        
+    Returns
+    -------
+    h_0 : array-like, shape = [N]
+        Daubechies' scaling filter 
+    h_1 : array-like, shape = [N]
+        Daubechies' wavelet filter 
 
-    Output: 
-       h_0 : Minimal phase Daubechies' scaling filter 
-       h_1 : Minimal phase Daubechies' wavelet filter 
+    Example
+    -------
+        h_0, h_1 = daubcqf(N=4, TYPE='min')
+        print h_0, h_1
+        [0.4830, 0.8365, 0.2241, -0.1294], [0.1294, 0.2241, -0.8365, 0.4830]
 
-    Example:
-       N = 4;
-       TYPE = 'min';
-       [h_0,h_1] = daubcqf(N,TYPE)
-       h_0 = 0.4830 0.8365 0.2241 -0.1294
-       h_1 = 0.1294 0.2241 -0.8365 0.4830
-
-    Reference: "Orthonormal Bases of Compactly Supported Wavelets",
-                CPAM, Oct.89 
+    Reference
+    ---------
+        "Orthonormal Bases of Compactly Supported Wavelets", CPAM, Oct.89 
     """
 
     assert N%2==0, 'No Daubechies filter exists for ODD length'
--- src/cyrwt.pyx
+++ src/cyrwt.pyx
@@ -57,45 +57,55 @@ def _prepareInputs(src_array, h, L):
     #
     if m > 1:
         mtest = m / 2.0**L
-        assert(mtest == int(mtest), "The matrix row dimension must be of size m*2^(L)")
+        assert(mtest == int(mtest), "The matrix row dimension must be an integer multiplication of 2**L")
 
     #
     # Check the COLUMN dimension of input
     #
     if n > 1:
         ntest = n / 2.0**L
-        assert(ntest == int(ntest), "The matrix column dimension must be of size n*2^(L)")
+        assert(ntest == int(ntest), "The matrix column dimension must be an integer multiplication of 2**L")
         
     return src_array, L, m, n, lh
 
 
 def mdwt(x, h, L=None):
     """	
-    Function computes the discrete wavelet transform y for a 1D or 2D input
+    Computes the discrete wavelet transform y for a 1D or 2D input
     signal x using the scaling filter h.
 
-    Input:
-       x : finite length 1D or 2D signal (implicitly periodized)
-       h : scaling filter
-       L : number of levels. In the case of a 1D signal, length(x) must be
-           divisible by 2^L; in the case of a 2D signal, the row and the
-           column dimension must be divisible by 2^L. If no argument is
-           specified, a full DWT is returned for maximal possible L.
+    Parameters
+    ----------
+    x : array-like, shape = [n] or [m, n]
+        Finite length 1D or 2D signal (implicitly periodized)
+    h : array-like, shape = [n]
+        Scaling filter
+    L : integer, optional (default=None)
+        Number of levels. In the case of a 1D signal, length(x) must be
+        divisible by 2^L; in the case of a 2D signal, the row and the
+        column dimension must be divisible by 2^L. If no argument is
+        specified, a full DWT is returned for maximal possible L.
 
-    Output:
-       y : the wavelet transform of the signal 
-           (see example to understand the coefficients)
-       L : number of decomposition levels
+    Returns
+    -------
+    y : array-like, shape = [n] or [m, n]
+        The wavelet transform of the signal 
+        (see example to understand the coefficients)
+    L : integer
+	number of decomposition levels
 
+    Examples
+    --------
     1D Example:
-       x = makesig('LinChirp',8);
-       h = daubcqf(4,'min');
-       L = 2;
-       [y,L] = mdwt(x,h,L)
+    
+       x = makesig('LinChirp', 8)
+       h = daubcqf(4, 'min')[0]
+       L = 2
+       y, L = mdwt(x, h, L)
 
-    1D Example's  output and explanation:
+    1D Example's output and explanation:
 
-       y = [1.1097 0.8767 0.8204 -0.5201 -0.0339 0.1001 0.2201 -0.1401]
+       y = [1.1097, 0.8767, 0.8204, -0.5201, -0.0339, 0.1001, 0.2201, -0.1401]
        L = 2
 
     The coefficients in output y are arranged as follows
@@ -106,12 +116,11 @@ def mdwt(x, h, L=None):
 
     2D Example:
 
-       load test_image        
-       h = daubcqf(4,'min');
-       L = 1;
-       [y,L] = mdwt(test_image,h,L);
+       h = daubcqf(4, 'min')
+       L = 1
+       y, L = mdwt(test_image, h, L)
 
-    2D Example's  output and explanation:
+    2D Example's output and explanation:
 
        The coefficients in y are arranged as follows.
 
@@ -136,7 +145,9 @@ def mdwt(x, h, L=None):
             4 : Low pass vertically and Low pass horizontally 
                 (scaling coefficients)
 
-    See also: midwt, mrdwt, mirdwt
+    See also
+    --------
+    midwt, mrdwt, mirdwt
 
     """
     
@@ -164,36 +175,46 @@ def mdwt(x, h, L=None):
 
 def midwt(y, h, L=None):
     """
-    Function computes the inverse discrete wavelet transform x for a 1D or
+    Computes the inverse discrete wavelet transform x for a 1D or
     2D input signal y using the scaling filter h.
 
-    Input:
-	y : finite length 1D or 2D input signal (implicitly periodized)
-           (see function mdwt to find the structure of y)
-       h : scaling filter
-       L : number of levels. In the case of a 1D signal, length(x) must be
-           divisible by 2^L; in the case of a 2D signal, the row and the
-           column dimension must be divisible by 2^L.  If no argument is
-           specified, a full inverse DWT is returned for maximal possible
-           L.
+    Parameters
+    ----------
+    y : array-like, shape = [n] or [m, n]
+        Finite length 1D or 2D input signal (implicitly periodized)
+	(see function mdwt to find the structure of y)
+    h : array-like, shape = [n]
+        Scaling filter
+    L : integer, optional (default=None)
+        Number of levels. In the case of a 1D signal, length(x) must be
+        divisible by 2^L; in the case of a 2D signal, the row and the
+        column dimension must be divisible by 2^L. If no argument is
+        specified, a full DWT is returned for maximal possible L.
 
-    Output:
-       x : periodic reconstructed signal
-       L : number of decomposition levels
+    Returns
+    -------
+    x : array-like, shape = [n] or [m, n]
+        Periodic reconstructed signal
+    L : integer
+	Number of decomposition levels
 
+    Examples
+    --------
     1D Example:
-       xin = makesig('LinChirp',8);
-       h = daubcqf(4,'min');
-       L = 1;
-       [y,L] = mdwt(xin,h,L);
-       [x,L] = midwt(y,h,L)
+       xin = makesig('LinChirp', 8)
+       h = daubcqf(4, 'min')[0]
+       L = 1
+       y, L = mdwt(xin, h, L)
+       x, L = midwt(y, h, L)
 
     1D Example's  output:
 
-       x = 0.0491 0.1951 0.4276 0.7071 0.9415 0.9808 0.6716 0.0000
+       x = [0.0491, 0.1951, 0.4276, 0.7071, 0.9415, 0.9808, 0.6716, 0.0000]
        L = 1
 
-    See also: mdwt, mrdwt, mirdwt
+    See also
+    --------
+    mdwt, mrdwt, mirdwt
     """
     
     y, L, m, n, lh = _prepareInputs(y, h, L)
@@ -220,9 +241,7 @@ def midwt(y, h, L=None):
 
 def mrdwt(x, h, L=None):
     """
-    [yl,yh,L] = mrdwt(x,h,L);
- 
-    Function computes the redundant discrete wavelet transform y
+    Computes the redundant discrete wavelet transform y
     for a 1D  or 2D input signal. (Redundant means here that the
     sub-sampling after each stage is omitted.) yl contains the
     lowpass and yh the highpass components. In the case of a 2D
@@ -230,60 +249,34 @@ def mrdwt(x, h, L=None):
     [lh hl hh lh hl ... ] (first letter refers to row, second to
     column filtering). 
 
-    Input:
-       x : finite length 1D or 2D signal (implicitly periodized)
-       h : scaling filter
-       L : number of levels. In the case of a 1D 
-           length(x) must be  divisible by 2^L;
-           in the case of a 2D signal, the row and the
-           column dimension must be divisible by 2^L.
-           If no argument is
-           specified, a full DWT is returned for maximal possible L.
-   
-    Output:
-       yl : lowpass component
-       yh : highpass components
-       L  : number of levels
+    Parameters
+    ----------
+    x : array-like, shape = [n] or [m, n]
+        Finite length 1D or 2D signal (implicitly periodized)
+    h : array-like, shape = [n]
+        Scaling filter
+    L : integer, optional (default=None)
+        Number of levels. In the case of a 1D signal, len(x) must be
+        divisible by 2**L; in the case of a 2D signal, the row and the
+        column dimension must be divisible by 2**L. If no argument is
+        specified, a full DWT is returned for maximal possible L.
 
-  HERE'S AN EASY WAY TO RUN THE EXAMPLES:
-  Cut-and-paste the example you want to run to a new file 
-  called ex.m, for example. Delete out the  at the beginning 
-  of each line in ex.m (Can use search-and-replace in your editor
-  to replace it with a space). Type 'ex' in matlab and hit return.
+    Returns
+    -------
+    yl : array-like, shape = [n] or [m, n]
+        Lowpass component
+    yh : array-like, shape = [n] or [m, n]
+        Highpass component
+    L : integer
+	number of decomposition levels
+	
+    See also
+    --------
+    mdwt, midwt, mirdwt
 
-
-    Example 1::
-    x = makesig('Leopold',8);
-    h = daubcqf(4,'min');
-    L = 1;
-    [yl,yh,L] = mrdwt(x,h,L)
-    yl =  0.8365  0.4830 0 0 0 0 -0.1294 0.2241
-    yh = -0.2241 -0.1294 0 0 0 0 -0.4830 0.8365
-    L = 1
-    Example 2:
-    load lena;
-    h = daubcqf(4,'min');
-    L = 2;
-    [ll_lev2,yh,L] = mrdwt(lena,h,L);  lena is a 256x256 matrix
-    N = 256;
-    lh_lev1 = yh(:,1:N); 
-    hl_lev1 = yh(:,N+1:2*N); 
-    hh_lev1 = yh(:,2*N+1:3*N);
-    lh_lev2 = yh(:,3*N+1:4*N); 
-    hl_lev2 = yh(:,4*N+1:5*N); 
-    hh_lev2 = yh(:,5*N+1:6*N);
-    figure; colormap(gray); imagesc(lena); title('Original Image');
-    figure; colormap(gray); imagesc(ll_lev2); title('LL Level 2');
-    figure; colormap(gray); imagesc(hh_lev2); title('HH Level 2');
-    figure; colormap(gray); imagesc(hl_lev2); title('HL Level 2');
-    figure; colormap(gray); imagesc(lh_lev2); title('LH Level 2');
-    figure; colormap(gray); imagesc(hh_lev1); title('HH Level 1');
-    figure; colormap(gray); imagesc(hl_lev2); title('HL Level 1');
-    figure; colormap(gray); imagesc(lh_lev2); title('LH Level 1');
-           
-    See also: mdwt, midwt, mirdwt
-
-    Warning! min(size(x))/2^L should be greater than length(h)
+    Warning
+    -------
+    min(x.shape)/2**L should be greater than len(h)
 
     """
 
@@ -318,7 +311,7 @@ def mrdwt(x, h, L=None):
 
 def mirdwt(yl, yh, h, L=None):
     """
-    Function computes the inverse redundant discrete wavelet
+    Computes the inverse redundant discrete wavelet
     transform x  for a 1D or 2D input signal. (Redundant means here
     that the sub-sampling after each stage of the forward transform
     has been omitted.) yl contains the lowpass and yl the highpass
@@ -327,62 +320,34 @@ def mirdwt(yl, yh, h, L=None):
     yh is [lh hl hh lh hl ... ] (first letter refers to row, second
     to column filtering).  
 
-    Input:
-       yl : lowpass component
-       yh : highpass components
-       h  : scaling filter
-       L  : number of levels. In the case of a 1D signal, 
-            length(yl) must  be divisible by 2^L;
-            in the case of a 2D signal, the row and
-            the column dimension must be divisible by 2^L.
+    Parameters
+    ----------
+    yl : array-like, shape = [n] or [m, n]
+        Lowpass component
+    yh : array-like, shape = [n] or [m, n]
+        Highpass component
+    h : array-like, shape = [n]
+        Scaling filter
+    L : integer
+	number of levels. In the case of a 1D signal, 
+	len(yl) must  be divisible by 2**L;
+	in the case of a 2D signal, the row and
+	the column dimension must be divisible by 2**L.	
    
-    Output:
-	     x : finite length 1D or 2D signal
-	     L : number of levels
+    Returns
+    -------
+    x : array-like, shape = [n] or [m, n]
+        Finite length 1D or 2D signal
+    L : integer
+	number of decomposition levels
 
-  HERE'S AN EASY WAY TO RUN THE EXAMPLES:
-  Cut-and-paste the example you want to run to a new file 
-  called ex.m, for example. Delete out the  at the beginning 
-  of each line in ex.m (Can use search-and-replace in your editor
-  to replace it with a space). Type 'ex' in matlab and hit return.
+    See also
+    --------
+    mdwt, midwt, mrdwt
 
-
-    Example 1:
-    xin = makesig('Leopold',8);
-    h = daubcqf(4,'min');
-    L = 1;
-    [yl,yh,L] = mrdwt(xin,h,L);
-    [x,L] = mirdwt(yl,yh,h,L)
-    x = 0.0000 1.0000 0.0000 -0.0000 0 0 0 -0.0000
-    L = 1
-  
-    Example 2:  
-    load lena;
-    h = daubcqf(4,'min');
-    L = 2;
-    [ll_lev2,yh,L] = mrdwt(lena,h,L);  lena is a 256x256 matrix
-    N = 256;
-    lh_lev1 = yh(:,1:N); 
-    hl_lev1 = yh(:,N+1:2*N); 
-    hh_lev1 = yh(:,2*N+1:3*N);
-    lh_lev2 = yh(:,3*N+1:4*N); 
-    hl_lev2 = yh(:,4*N+1:5*N); 
-    hh_lev2 = yh(:,5*N+1:6*N);
-    figure; colormap(gray); imagesc(lena); title('Original Image');
-    figure; colormap(gray); imagesc(ll_lev2); title('LL Level 2');
-    figure; colormap(gray); imagesc(hh_lev2); title('HH Level 2');
-    figure; colormap(gray); imagesc(hl_lev2); title('HL Level 2');
-    figure; colormap(gray); imagesc(lh_lev2); title('LH Level 2');
-    figure; colormap(gray); imagesc(hh_lev1); title('HH Level 1');
-    figure; colormap(gray); imagesc(hl_lev2); title('HL Level 1');
-    figure; colormap(gray); imagesc(lh_lev2); title('LH Level 1');
-    [lena_Hat,L] = mirdwt(ll_lev2,yh,h,L);
-    figure; colormap(gray); imagesc(lena_Hat); 
-                            title('Reconstructed Image');
-
-    See also: mdwt, midwt, mrdwt
-
-    Warning! min(size(yl))/2^L should be greater than length(h)
+    Warning
+    -------
+    min(yl.shape)/2**L should be greater than len(h)
 
     """
 
